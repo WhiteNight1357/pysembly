36640d212e0895899c26d5c0667867f168ea5083
whitenight1357 <notwhitenight1357@gmail.com>
2022-05-12 21:09:06 +0900

    New parameter: 'mode' for instant/direct/indirect addressing

--- main.py
+++ main.py
@@ -7,35 +7,35 @@ class PyAsmScript:
         self.accumulator = 0
         self.stack = []
 
-    def load(self, location):
-        self.script.append(["load", location])
+    def load(self, location, mode):
+        self.script.append(["load", location, mode])
 
-    def store(self, location):
-        self.script.append(["store", location])
+    def store(self, location, mode):
+        self.script.append(["store", location, mode])
 
-    def goto(self, location):
-        self.script.append(["goto", location])
+    def goto(self, location, mode):
+        self.script.append(["goto", location, mode])
 
-    def add(self, location):
-        self.script.append(["add", location])
+    def add(self, location, mode):
+        self.script.append(["add", location, mode])
 
-    def sub(self, location):
-        self.script.append(["sub", location])
+    def sub(self, location, mode):
+        self.script.append(["sub", location, mode])
 
-    def compare_more(self, location):
-        self.script.append(["cmp_more", location])
+    def compare_more(self, location, mode):
+        self.script.append(["cmp_more", location, mode])
 
-    def compare_less(self, location):
-        self.script.append(("cmp_less", location))
+    def compare_less(self, location, mode):
+        self.script.append(["cmp_less", location, mode])
 
-    def compare_equal(self, location):
-        self.script.append(["cmp_equal", location])
+    def compare_equal(self, location, mode):
+        self.script.append(["cmp_equal", location, mode])
 
-    def push(self, location):
-        self.script.append(["push", location])
+    def push(self, location, mode):
+        self.script.append(["push", location, mode])
 
     def pop(self):
-        self.script.append(["pop", None])
+        self.script.append(["pop", None, None])
 
     def run(self):
 
@@ -47,66 +47,52 @@ class PyAsmScript:
 
             code = self.script[self.scriptpointer][0]
             location = self.script[self.scriptpointer][1]
+            mode = self.script[self.scriptpointer][2]
+
+            if mode is None:
+                value = location
+            elif mode is True:
+                value = self.memory[location]
+            elif mode is False:
+                value = self.memory[self.memory[location]]
+            else:
+                raise Exception("'mode' should be None, True, or False")
 
             if code == "load":
-                try:
-                    self.accumulator = self.memory[location]
-                except TypeError:
-                    self.accumulator = int(location)
+                self.accumulator = value
 
             if code == "store":
-                while len(self.memory) < location + 1:
+                while len(self.memory) < value + 1:
                     self.memory.append(None)
-                self.memory.pop(location)
-                self.memory.insert(location, self.accumulator)
+                self.memory.pop(value)
+                self.memory.insert(value, self.accumulator)
 
             if code == "goto":
-                if location is None:
+                if value is None:
                     self.scriptpointer = self.accumulator
                 else:
-                    self.scriptpointer = location
+                    self.scriptpointer = value
 
             if code == "add":
-                try:
-                    self.accumulator += self.memory[location]
-                except TypeError:
-                    self.accumulator += int(location)
+                self.accumulator += value
 
             if code == "sub":
-                try:
-                    self.accumulator -= self.memory[location]
-                except TypeError:
-                    self.accumulator -= int(location)
+                self.accumulator -= value
 
             if code == "cmp_more":
-                try:
-                    if not self.accumulator > self.memory[location]:
-                        self.scriptpointer += 1
-                except TypeError:
-                    if not self.accumulator > int(location):
-                        self.scriptpointer += 1
+                if not self.accumulator > value:
+                    self.scriptpointer += 1
 
             if code == "cmp_less":
-                try:
-                    if not self.accumulator < self.memory[location]:
-                        self.scriptpointer += 1
-                except TypeError:
-                    if not self.accumulator < int(location):
-                        self.scriptpointer += 1
+                if not self.accumulator < value:
+                    self.scriptpointer += 1
 
             if code == "cmp_equal":
-                try:
-                    if not self.accumulator == self.memory[location]:
-                        self.scriptpointer += 1
-                except TypeError:
-                    if not self.accumulator == int(location):
-                        self.scriptpointer += 1
+                if not self.accumulator == value:
+                    self.scriptpointer += 1
 
             if code == "push":
-                try:
-                    self.stack.append(self.memory[location])
-                except TypeError:
-                    self.stack.append(int(location))
+                self.stack.append(value)
 
             if code == "pop":
                 self.accumulator = self.stack.pop()
@@ -126,25 +112,25 @@ class PyAsmScript:
 if __name__ == "__main__":
     asm = PyAsmScript()
 
-    asm.load("0")               # 0:
-    asm.store(0)                # 1: set location 0 as int 0 (location 0 is var 'first' from now on)
-    asm.load("1")               # 2:
-    asm.store(1)                # 3: set location 1 as int 1 (location 1 is var 'second' from now on)
-    asm.load("0")               # 4:
-    asm.store(2)                # 5: set location 2 as int 0 (location 2 is var 'next' from now on)
+    asm.load(0, None)                  # 0:
+    asm.store(0, None)                 # 1: set location 0 as int 0 (location 0 is var 'first' from now on)
+    asm.load(1, None)                  # 2:
+    asm.store(1, None)                 # 3: set location 1 as int 1 (location 1 is var 'second' from now on)
+    asm.load(0, None)                  # 4:
+    asm.store(2, None)                 # 5: set location 2 as int 0 (location 2 is var 'next' from now on)
 
-    asm.load(0)                 # 6: add first and second to
-    asm.add(1)                  # 7: calculate next
-    asm.store(2)                # 8:
+    asm.load(0, True)                  # 6: add first and second to
+    asm.add(1, True)                   # 7: calculate next
+    asm.store(2, None)                 # 8:
 
-    asm.load(1)                 # 9: move second to first
-    asm.store(0)                # 10:
-    asm.load(2)                 # 11: move next to second
-    asm.store(1)                # 12:
+    asm.load(1, True)                  # 9: move second to first
+    asm.store(0, None)                 # 10:
+    asm.load(2, True)                  # 11: move next to second
+    asm.store(1, None)                 # 12:
 
-    asm.compare_less("200")     # 13: if second is less than int 200
-    asm.goto(6)                 # 14: goto 6 and repeat
+    asm.compare_less(200, None)        # 13: if second is less than int 200
+    asm.goto(6, None)                  # 14: goto 6 and repeat
 
     asm.run()
 
-    print(asm.memory[1])        # print(second)
+    print(asm.memory[1])               # print(second)
