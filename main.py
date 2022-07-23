80e2f5cc78c0bc9900394956b91a04e7008c1406
whitenight1357 <notwhitenight1357@gmail.com>
2022-07-23 10:42:53 +0900

    Made label_location Changed label to label_line

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ class PyAsmScript:
     def pop(self):
         self.script.append(["pop", None, None])
 
-    def label(self, label):     # label() labels NEXT line, not previous
+    def label_line(self, label):     # label_line() labels NEXT line, not previous
         try:
             self.labels.index(label)
             raise Exception("this label is already in label list")
@@ -49,6 +49,16 @@ class PyAsmScript:
                 raise Exception("'label' should be string")
             self.labels.append(label)
             self.labeldata.append(len(self.script))
+
+    def label_location(self, location, label):   # label_location() does not count as script
+        try:
+            self.labels.index(label)
+            raise Exception("this label is already in label list")
+        except ValueError:
+            if not isinstance(label, str):
+                raise Exception("'label' should be string")
+            self.labels.append(label)
+            self.labeldata.append(location)
 
     def null(self):
         self.script.append([None, None, None])
@@ -64,6 +74,9 @@ class PyAsmScript:
             code = self.script[self.scriptpointer][0]
             location = self.script[self.scriptpointer][1]
             mode = self.script[self.scriptpointer][2]
+
+            if isinstance(location, str):
+                location = self.labeldata[self.labels.index(location)]
 
             if mode is None:
                 value = location
@@ -138,26 +151,30 @@ class PyAsmScript:
 
 
 if __name__ == "__main__":
-    asm = PyAsmScript()
-    asm.debugging = True
+    asm = PyAsmScript()                # Sample Code: Calculates first fibonacci number higher than 200
+    asm.debugging = True               # Enables debugging
+
+    asm.label_location(0, "first")     # label: location 0 as label "first"
+    asm.label_location(1, "second")    # label: location 1 as label "second"
+    asm.label_location(2, "next")      # label: location 2 as label "next"
 
     asm.load(0, None)                  # 0:
-    asm.store(0, None)                 # 1: set location 0 as int 0 (location 0 is var 'first' from now on)
+    asm.store("first", None)           # 1: set first as int 0
     asm.load(1, None)                  # 2:
-    asm.store(1, None)                 # 3: set location 1 as int 1 (location 1 is var 'second' from now on)
+    asm.store("second", None)          # 3: set second as int 1
     asm.load(0, None)                  # 4:
-    asm.store(2, None)                 # 5: set location 2 as int 0 (location 2 is var 'next' from now on)
+    asm.store("next", None)            # 5: set next as int 0
 
-    asm.label("a")                     # label: line 6 as label "a"
+    asm.label_line("a")                # label: line 6 as label "a"
 
-    asm.load(0, True)                  # 6: add first and second to
-    asm.add(1, True)                   # 7: calculate next
-    asm.store(2, None)                 # 8:
+    asm.load("first", True)            # 6: add first and second to
+    asm.add("second", True)            # 7: calculate next
+    asm.store("next", None)            # 8:
 
-    asm.load(1, True)                  # 9: move second to first
-    asm.store(0, None)                 # 10:
-    asm.load(2, True)                  # 11: move next to second
-    asm.store(1, None)                 # 12:
+    asm.load("second", True)           # 9: move second to first
+    asm.store("first", None)           # 10:
+    asm.load("next", True)             # 11: move next to second
+    asm.store("second", None)          # 12:
 
     asm.compare_less(200, None)        # 13: if second is less than int 200
     asm.goto("a", None)                # 14: goto "a" and repeat
